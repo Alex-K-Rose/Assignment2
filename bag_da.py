0ecbb76e9d99291ac08c31c1edb05e7806f8dd6e
kavvik23 <rosea2@oregonstate.edu>
2022-02-10 08:33:59 -0800

    update

--- bag_da.py
+++ bag_da.py
@@ -3,10 +3,11 @@
 # Course: CS261 - Data Structures
 # Assignment: 2
 # Due Date: 1/31/2022
-# Description:
+# Description: Part 2 of Assignment two building out methods for Bag class which
+# will use a DynamicArray object as its underlying data storage container and
+# will make use of some DynamicArray methods for its' own methods.
 
 from dynamic_array import *
-
 
 class Bag:
     def __init__(self, start_bag=None):
@@ -43,45 +44,91 @@ class Bag:
 
     def add(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        This method adds a new element to the bag
         """
-        pass
+        self.da.append(value)
 
     def remove(self, value: object) -> bool:
         """
-        TODO: Write this implementation
+        This method removes any one element from the bag that matches the provided value
+        object.
         """
-        pass
+        for item in range(self.size()):
+            if self.da[item]==value:
+                self.da.remove_at_index(item)
+                return True
+
+        return False
 
     def count(self, value: object) -> int:
         """
-        TODO: Write this implementation
+        This method returns the number of elements in the bag that match the provided value
+        object.
         """
-        pass
+        count=0
+        for item in range(self.size()):
+            if value==self.da[item]:
+                count+=1
+
+        return count
 
     def clear(self) -> None:
         """
-        TODO: Write this implementation
+        This method clears the contents of the bag
         """
-        pass
+        self.da=DynamicArray() #abandon original array
 
     def equal(self, second_bag: object) -> bool:
         """
-        TODO: Write this implementation
+        This method compares the contents of a bag with the content of a second bag provided as a
+        parameter. The method returns True if the bags are equal (have the same number of
+        elements and contain the same elements without regards to the order of elements).
+        Otherwise, it returns False
         """
-        pass
+        if self.size()==0 and second_bag.size()==0:
+            return True
+        elif self.size() != second_bag.size():
+            return False
+
+        bag_copy1=Bag(self) #copy bags and set a counter to 0
+        bag_copy2=Bag(second_bag)
+        replacement_count=0
+
+        for item1 in range(self.size()): #compare each item in bag1 to bag2 if equal to each other and not None set equal to None
+            for item2 in range(second_bag.size()):
+                if bag_copy1.da[item1]==bag_copy2.da[item2] and bag_copy1.da[item1] != None :
+                    bag_copy1.da[item1]=None
+                    bag_copy2.da[item2]=None
+                    replacement_count+=1   #count how many items have been replaced
+
+        if replacement_count==self.size() and replacement_count==second_bag.size(): #if number of replacements is equal to size all elements have been replaced
+            return True
+
+        else:
+            return False
+
 
     def __iter__(self):
         """
-        TODO: Write this implementation
+        This method enables the Bag to iterate across itself.
         """
-        pass
+        self.index = 0
+
+        return self
 
     def __next__(self):
         """
-        TODO: Write this implementation
+        This method will return the next item in the Bag, based on the current location of the
+        iterator
         """
-        pass
+        try:
+            value = self.da[self.index]
+
+        except DynamicArrayException:
+            raise StopIteration
+
+        self.index = self.index + 1
+        return value
 
 
 # ------------------- BASIC TESTING -----------------------------------------
@@ -150,3 +197,4 @@ if __name__ == "__main__":
     print(bag)
     for item in bag:
         print(item)
+
